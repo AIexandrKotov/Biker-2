99fb39d2a6d7e7fef548df567cca427d60d96492
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2019-01-30 01:33:38 +0300

    Add files via upload

---
+++ Biker2Installer.pas
@@ -0,0 +1,223 @@
+﻿{$apptype console}
+{$product Biker 2 Installer}
+{$company Kotov Projects}
+
+{$copyright Alexandr Kotov}
+
+{$resource 'interface.dat'}
+{$resource 'help.dat'}
+{$resource 'map.dat'}
+{$resource 'items.dat'}
+{$resource 'quest.dat'}
+{$resource 'shop.dat'}
+{$resource 'mapview.exe'}
+{$resource 'game.exe'}
+{$resource 'bike.dat'}
+{$resource 'map.png'}
+{$resource 'version.dat'}
+
+uses System;
+
+const
+  version: record Major, Minor, Build: integer; end = (Major: 0; Minor: 9; Build: 34);
+  LANGS = 2;
+  
+type
+  LANG = (RUS, ENG);
+  
+function IntToLang(a: integer): LANG := LANG(a);
+
+function LangToInt(a: LANG): integer := Ord(a);
+
+procedure Change(var a: boolean);
+begin
+  if a then a:=false
+  else a:=true;
+end;
+
+procedure Resource(ResName, Outputfilename: string);
+begin
+  var f := System.IO.File.Create(Outputfilename);
+  GetResourceStream(ResName).CopyTo(f);
+  f.Flush;
+  f.Close;
+end;
+
+var
+
+  CurrentLang: LANG = RUS;
+
+  Res: array of string = Arr(
+  'game.exe',
+  'mapview.exe',
+  'map.dat',
+  'interface.dat',
+  'bike.dat',
+  'shop.dat',
+  'items.dat',
+  'quest.dat',
+  'help.dat',//8 0
+  'map.png',//9 1
+  'version.dat'//10 2
+  );
+  
+  License: array[LANG] of string =(
+    #10'[RUS]'
+    #10'--- Лицензионное соглашение ---'
+    #10''
+    #10'Что можно:'
+    #10' - Использовать'
+    #10' - Распространять под изначальным именем с указанием автора и следующих ссылок:'
+    #10'     vk.com/id219453333 (Александр Котов)'
+    #10'     vk.com/ktvprj (Kotov Projects)'
+    #10'     Электронной почты kotov.tv20012@gmail.com',
+    #10'[ENG]'
+    #10'--- License agreement ---'
+    #10''
+    #10'What can:'
+    #10' - Use'
+    #10' - Distribute under the original name with the author and the links:'
+    #10'    vk.com/id219453333 (Alexandr Kotov)'
+    #10'    vk.com/ktvprj (Kotov Projects)'
+    #10'    E-mail kotov.tv20012@gmail.com');
+
+  IFace: array[LANG] of array of string =(
+  ('RUS',
+    'Мастер установки Biker 2',
+    'Версия',
+    'Да','Нет',//3,4
+    'Установить',
+    'Лицензия',
+    'Отмена',
+    'Параметры установки',
+    'Установить справку по игре',
+    'Карта мира (.png)',
+    'Список обновлений'
+  ),
+  ('ENG',
+    'Biker 2 Installer',
+    'Version',
+    'Yes','No',//3,4
+    'Install',
+    'License',
+    'Cancel',
+    'Installation settings',
+    'Install more information on the game',
+    'World Map (.png)',
+    'Update list'
+  )
+  );
+
+begin  
+  var status: boolean = true;
+  var arr:= new boolean[3];
+  arr[0]:=true;
+  arr[1]:=true;
+  arr[2]:=false;
+  Console.BackgroundColor:=consolecolor.White;
+  Console.ForegroundColor:=consolecolor.Black;
+  Console.SetWindowSize(1,1);
+  Console.SetBufferSize(100,30);
+  Console.SetWindowSize(100,30);
+  while status do
+  begin
+    var YES := IFace[CurrentLang][3];
+    var NO  := IFace[CurrentLang][4];
+    if Console.Title<>IFace[CurrentLang][1] then Console.Title:=IFace[CurrentLang][1];
+    if ((Console.WindowWidth<>100) or (Console.WindowHeight<>30)) then
+    begin
+      Console.Clear;
+      if (console.WindowHeight>30) or (console.WindowWidth>100) then Console.SetWindowSize(1,1);
+      Console.SetBufferSize(100,30);
+      Console.SetWindowSize(100,30);
+    end;
+    Console.Clear;
+    
+    var aaa: string = $'(-) {IFace[CurrentLang][0]}';
+    Console.SetCursorPosition(99-aaa.Length,29);write(aaa);
+    
+    Console.SetCursorPosition(1,1);write('Biker 2 (',IFace[CurrentLang][2],' ',Version.Major,'.',Version.Minor,'.',Version.Build,')');
+    
+    Console.SetCursorPosition(1,3);write('(1) ',IFace[CurrentLang][5]);
+    Console.SetCursorPosition(1,4);write('(2) ',IFace[CurrentLang][6]);
+    Console.SetCursorPosition(1,5);write('(0) ',IFace[CurrentLang][7]);
+    
+    Console.SetCursorPosition(48,1);write(IFace[CurrentLang][8],': ');
+    Console.SetCursorPosition(50,2);write('(-1) ',IFace[CurrentLang][9],': ');
+    if arr[0] then
+    begin
+      Console.ForegroundColor:=ConsoleColor.DarkGreen;
+      write(YES);
+      Console.ForegroundColor:=ConsoleColor.Black;
+    end
+    else
+    begin
+      Console.ForegroundColor:=ConsoleColor.Red;
+      write(NO);
+      Console.ForegroundColor:=ConsoleColor.Black;
+    end;
+    Console.SetCursorPosition(50,3);write('(-2) ',IFace[CurrentLang][10],': ');
+    if arr[1] then
+    begin
+      Console.ForegroundColor:=ConsoleColor.DarkGreen;
+      write(YES);
+      Console.ForegroundColor:=ConsoleColor.Black;
+    end
+    else
+    begin
+      Console.ForegroundColor:=ConsoleColor.Red;
+      write(NO);
+      Console.ForegroundColor:=ConsoleColor.Black;
+    end;
+    Console.SetCursorPosition(50,4);write('(-3) ',IFace[CurrentLang][11],': ');
+    if arr[2] then
+    begin
+      Console.ForegroundColor:=ConsoleColor.DarkGreen;
+      write(YES);
+      Console.ForegroundColor:=ConsoleColor.Black;
+    end
+    else
+    begin
+      Console.ForegroundColor:=ConsoleColor.Red;
+      write(NO);
+      Console.ForegroundColor:=ConsoleColor.Black;
+    end;
+    
+    var input: string = '';
+    while (input='') and ((Console.WindowWidth=100) and (Console.WindowHeight=30)) do
+    begin
+      Console.SetCursorPosition(1,28);write(': ');
+      input:=ReadLnString;
+    end;
+    
+    if input='-1' then Change(arr[0]);
+    if input='-2' then Change(arr[1]);
+    if input='-3' then Change(arr[2]);
+    if input='-' then
+    begin
+      CurrentLang:=(IntToLang(LangToInt(CurrentLang)+1));
+      if LangToInt(CurrentLang)>=Langs then CurrentLang:=IntToLang(0);
+    end;
+    if input='0' then status:=false;
+    if input='2' then
+    begin
+      Console.Clear;write(License[CurrentLang]);Console.CursorVisible:=false;
+      Console.ReadKey;;Console.CursorVisible:=true;
+    end;
+    if input.ToLower='1' then
+    begin
+      MkDir('Biker 2');
+      for var i:=0 to Res.Length-1 do
+      begin
+        if (not ((i=8) and (not arr[0])))
+        and (not ((i=9) and (not arr[1])))
+        and (not ((i=10) and (not arr[2]))) then
+        begin
+          Resource(Res[i],string.Format('Biker 2\{0}',Res[i]));
+        end;
+      end;
+      //end;
+      status:=false;
+    end;
+  end;
+end.
---
+++ mapview.pas
@@ -0,0 +1,272 @@
+﻿{$apptype windows}
+{$product Biker 2 Map Viewer}
+{$version 1.0}
+{$company Kotov Projects}
+
+{$copyright Alexandr Kotov}
+
+//todo fixme
+
+uses GraphWPF, System;
+ 
+type
+  tpmap = record
+    name: string;
+    sort: integer;
+    x,y: real;
+    road: array of record
+      id: integer;
+      tp: char;
+    end;
+  end;
+ 
+var
+  types: array of string;
+  map: array of tpmap;
+ 
+procedure AddTypes(t : String);
+begin
+  var L := types.Count;
+  SetLength(types, L+1);
+  types[L] := t;
+end;
+  
+procedure ReadMap(fName : String);
+begin
+  if Not IO.File.Exists(fName)=true then Exit;
+  var txt := ReadAllText(fName, Encoding.UTF8).ToWords(#13#10.toArray);
+  var (mapid1, mapid2) := (0, 0);
+  foreach var s in txt do
+    if s.IndexOf('=') > 0 then
+      begin
+        var p := s.ToWords('=');
+        case p[0] of
+          'towns' : SetLength(map, p[1].ToInteger);
+          'newtype' : AddTypes(p[1]);
+          'town' : (mapid1, mapid2) := (StrToInt(p[1]), 0);
+          'name' : map[mapid1].name := p[1];
+          'type' : map[mapid1].sort := StrToInt(p[1]);
+          'posx' : map[mapid1].x := StrToFloat(p[1]);
+          'posy' : map[mapid1].y := StrToFloat(p[1]);
+          'road' : begin
+                     SetLength(map[mapid1].road, mapid2 + 1);
+                     map[mapid1].road[mapid2].id := StrToInt(p[1].ToWords(' ')[0]) - 1;
+                     map[mapid1].road[mapid2].tp := char.Parse(p[1].ToWords(' ')[1]);
+                     mapid2 += 1;
+                   end;
+        end;
+      end;
+  var maxy: real;
+  for var i:=0 to map.Length-1 do if maxy<map[i].y then maxy:=map[i].y;
+  for var i:=0 to map.Length-1 do map[i].y:=maxy-map[i].y;
+end;
+ 
+function Xcoord(X : Real; Town0, Town1 : tpmap; Scale : Real) := X + (Town1.x - Town0.x) * Scale;
+function Ycoord(Y : Real; Town0, Town1 : tpmap; Scale : Real) := Y + (Town1.y - Town0.y) * Scale;
+ 
+var Lout, Lwait : List<integer>; // out - отрисованные, wait - ожидающие отрисовки
+ 
+procedure DrawRoads(X, Y : Real; Town : tpmap; Scale : Real);
+begin
+  foreach var over in Town.road do
+    begin
+      case over.tp of
+        'B' : Pen.Color := Color.FromRgb(85,107,47);
+        'G' : Pen.Color := Color.FromRgb(255, 165, 0);
+      end;
+      Line(X, Y, Xcoord(X, Town, map[over.id], Scale), Ycoord(Y, Town, map[over.id], Scale));
+      if Not Lout.Contains(over.id) and Not Lwait.Contains(over.id) then Lwait.Add(over.id);
+    end;
+end;
+ 
+procedure DrawTown(X, Y : Real; DrawType : Integer; Town : tpmap; Scale : Real);
+begin
+  case DrawType of
+    0 : begin
+          DrawRoads(X, Y, Town, Scale);
+          Brush.Color := rgb(128, 128, 192);
+          Pen.Color := rgb(0, 0, 0);
+        end;
+    1 : begin
+          DrawRoads(X, Y, Town, Scale);
+          Brush.Color := rgb(128, 192, 128);
+          Pen.Color := rgb(0, 0, 0);
+        end;
+    2 : begin
+          DrawRoads(X, Y, Town, Scale);
+          Brush.Color := rgb(192, 128, 128);
+          Pen.Color := rgb(0, 0, 0);
+        end;
+    3 : begin // центральная точка
+          DrawRoads(X, Y, Town, Scale);
+          Brush.Color := rgb(255, 255, 0);
+          Pen.Color := rgb(255, 0, 0);
+          case Town.sort of
+            0: Circle(Round(X), Round(Y), 5);
+            1: Circle(Round(X), Round(Y), 8);
+            2: Circle(Round(X), Round(Y), 14);
+          end;
+          GraphWPF.TextOut(X, Y - 10 - TextHeight(Town.Name), Town.Name);
+          //DrawTextCentered(Round(X), Round(Y) - 10 - TextHeight(Town.Name), Town.Name);
+          Exit;
+        end;
+  end;
+  case Town.sort of
+    0: Circle(Round(X), Round(Y), 5);
+    1: Circle(Round(X), Round(Y), 8);
+    2: Circle(Round(X), Round(Y), 14);
+  end;
+  GraphWPF.TextOut(X, Y - 5 - TextHeight(Town.Name), Town.Name);
+end;
+ 
+procedure DrawMap(Center : Integer; Scale : Real);
+begin
+  Window.Clear;
+  Lout := New List<integer>;
+  Lwait := New List<integer>;
+  DrawTown(Window.Center.X, Window.Center.Y, 3, map[Center], Scale); Lout.Add(Center);
+  while Lwait.Count > 0 do
+    begin
+      var cur := Lwait.First;
+      if (Xcoord(Window.Center.X, map[Center], map[cur], Scale)>=0) and (Xcoord(Window.Center.X, map[Center], map[cur], Scale)<=window.Width)
+      and (Ycoord(Window.Center.Y, map[Center], map[cur], Scale)>=0) and (Ycoord(Window.Center.Y, map[Center], map[cur], Scale)<=window.Height) then
+      DrawTown(Xcoord(Window.Center.X, map[Center], map[cur], Scale), Ycoord(Window.Center.Y, map[Center], map[cur], Scale), map[cur].sort, map[cur], Scale);
+      Lout.Add(cur); Lwait.Remove(cur);
+    end;
+  Lout := nil;
+  Lwait := nil;
+end;
+
+var
+    minsc, maxsc, stepsc: real;
+    sc: real;
+    current: integer;
+    mxed: boolean;
+
+procedure MouseDown(x,y: real; n: integer);
+begin
+  if (n=1) and (sc<maxsc) then
+  begin
+    sc+=stepsc;
+    DrawMap(current, sc);
+  end;
+  if (n=2) and (sc>minsc) then
+  begin
+    sc-=stepsc;
+    DrawMap(current, sc);
+  end;
+end;
+
+procedure KeyPress(c : Char);
+begin
+  case c of
+     #27,'0' : Window.Close;
+     '1':
+      begin
+        if (sc<maxsc) then
+        begin
+          sc+=stepsc;
+          DrawMap(current, sc);
+        end;
+      end;
+     '2':
+      begin
+        if (sc>minsc) then
+        begin
+          sc-=stepsc;
+          DrawMap(current, sc);
+        end;
+      end;
+     '3':
+      begin
+        if mxed then
+        begin
+          window.Normalize;
+          mxed:=false;
+        end
+        else
+        begin
+          window.Maximize;
+          mxed:=true;
+        end;
+      end;
+  end;
+end;
+
+procedure KeyDown(k: key);
+begin
+  case k of
+    key.Escape, key.NumPad0: Window.Close;
+    key.NumPad1, key.Z:
+      begin
+        if (sc<maxsc) then
+        begin
+          sc+=stepsc;
+          DrawMap(current, sc);
+        end;
+      end;
+    key.NumPad2, key.X:
+      begin
+        if (sc>minsc) then
+        begin
+          sc-=stepsc;
+          DrawMap(current, sc);
+        end;
+      end;
+    key.C:
+      begin
+        if mxed then
+        begin
+          window.Normalize;
+          mxed:=false;
+        end
+        else
+        begin
+          window.Maximize;
+          mxed:=true;
+        end;
+      end;
+  end;
+end;
+
+procedure _Res := DrawMap(current, sc);
+
+ 
+begin
+  GraphWPF.Font.Name:='Arial';
+  if CommandLineArgs.Length=0 then
+  begin
+    window.Close;exit;
+  end
+  else if not (System.IO.File.Exists('map.dat')) then
+  begin
+    window.Close;exit;
+  end;
+  try
+    //current:=StrToInt(CommandLineArgs[0]);
+    var s:=CommandLineArgs;
+    current:=StrToInt(s[0]);
+    minsc:=StrToFloat(s[1]);
+    maxsc:=StrToFloat(s[2]);
+    stepsc:=StrToFloat(s[3]);
+  except
+    on e: System.Exception do
+    begin
+      window.Close;exit;
+    end;
+  end;
+  window.SetSize(500,500);
+  window.Title:='Biker 2 Map Viewer';
+  window.CenterOnScreen;
+  //window.IsFixedSize:=true;
+  //DrawInBuffer:=true;
+  SetLength(types, 0);
+  ReadMap('map.dat');
+  sc:=5;
+  //PABCSystem.CommandLineArgs
+  DrawMap(current, sc);
+  GraphWPF.OnMouseDown:=MouseDown;
+  GraphWPF.OnKeyPress:=KeyPress;
+  GraphWPF.OnKeyDown:=KeyDown;
+  GraphWPF.OnResize:=_Res;
+end.
